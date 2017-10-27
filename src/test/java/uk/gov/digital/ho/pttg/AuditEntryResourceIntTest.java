c6ead6ca5d0ca7ee74e192f18449ce2e99eb4392
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2017-10-27 09:19:09 +0100

    PTTG-519: Use SqlGroup to seed data

--- src/test/java/uk/gov/digital/ho/pttg/AuditEntryResourceIntTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/AuditEntryResourceIntTest.java
@@ -1,104 +1,40 @@
 package uk.gov.digital.ho.pttg;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.digital.ho.pttg.AuditEventType.INCOME_PROVING_FINANCIAL_STATUS_REQUEST;
-import static uk.gov.digital.ho.pttg.AuditEventType.INCOME_PROVING_FINANCIAL_STATUS_RESPONSE;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SqlGroup({
+        @Sql(executionPhase = BEFORE_TEST_METHOD, scripts = "/sql/AuditEntryResourceIntTest/before.sql"),
+        @Sql(executionPhase = AFTER_TEST_METHOD, scripts = "/sql/AuditEntryResourceIntTest/after.sql")
+})
 public class AuditEntryResourceIntTest {
 
-    private static final String SESSION_ID = "sessionID";
-    private static final String DEPLOYMENT = "deployment";
-    private static final String NAMESPACE = "env";
-
     @Autowired
-    private AuditEntryJpaRepository repository;
-
-    @Before
-    public void setup() {
-
-        repository.save(new AuditEntry(
-                "3a22c723-ea0f-4962-b97b-f35dce3284b2",
-                LocalDateTime.parse("2017-09-11T14:45:48.094"),
-                SESSION_ID,
-                "3a22c723-ea0f-4962-b97b-f35dce3284b2",
-                "bobby.bag@digital.homeoffice.gov.uk",
-                DEPLOYMENT,
-                NAMESPACE,
-                INCOME_PROVING_FINANCIAL_STATUS_REQUEST,
-                "{\n" +
-                        "      \"forename\": \"Antonio\",\n" +
-                        "      \"method\": \"get-financial-status\",\n" +
-                        "      \"dependants\": 0,\n" +
-                        "      \"surname\": \"Gramsci\",\n" +
-                        "      \"applicationRaisedDate\": \"2017-06-01\",\n" +
-                        "      \"dateOfBirth\": \"1891-01-22\",\n" +
-                        "      \"nino\": \"NE112233C\"\n" +
-                        "    }"
-        ));
-        repository.save(new AuditEntry(
-                "3a22c723-ea0f-4962-b97b-f35dce3284b2",
-                LocalDateTime.parse("2017-09-11T14:45:55.033"),
-                SESSION_ID,
-                "3a22c723-ea0f-4962-b97b-f35dce3284b2",
-                "bobby.bag@digital.homeoffice.gov.uk",
-                DEPLOYMENT,
-                NAMESPACE,
-                INCOME_PROVING_FINANCIAL_STATUS_RESPONSE,
-                "{\n" +
-                        "      \"method\": \"get-financial-status\",\n" +
-                        "      \"response\": {\n" +
-                        "        \"status\": {\n" +
-                        "          \"code\": \"100\",\n" +
-                        "          \"message\": \"OK\"\n" +
-                        "        },\n" +
-                        "        \"individual\": {\n" +
-                        "          \"title\": \"\",\n" +
-                        "          \"forename\": \"Antonio\",\n" +
-                        "          \"surname\": \"Gramsci\",\n" +
-                        "          \"nino\": \"NE112233C\"\n" +
-                        "        },\n" +
-                        "        \"categoryCheck\": {\n" +
-                        "          \"category\": \"A\",\n" +
-                        "          \"passed\": false,\n" +
-                        "          \"applicationRaisedDate\": \"2017-06-01\",\n" +
-                        "          \"assessmentStartDate\": \"2016-12-01\",\n" +
-                        "          \"failureReason\": \"MONTHLY_VALUE_BELOW_THRESHOLD\",\n" +
-                        "          \"threshold\": 1550,\n" +
-                        "          \"employers\": [\n" +
-                        "            \"MCDONALDS\"\n" +
-                        "          ]\n" +
-                        "        }\n" +
-                        "      }\n" +
-                        "    }"
-        ));
-    }
-
-    @After
-    public void tearDown() {
-        repository.deleteAll();
-    }
+    private TestRestTemplate restTemplate;
 
     @Test
     public void shouldRetrieveAllAudit() throws IOException, JSONException {
-//        String auditRecords = restTemplate.getForObject("/audit", String.class);
-//        String expectedRecords = IOUtils.toString(getClass().getResourceAsStream("/expected.json"));
-//
-//        JSONAssert.assertEquals(auditRecords, expectedRecords, false);
-        assertThat("blah".toUpperCase()).isEqualTo("BLAH");
+        String auditRecords = restTemplate.getForObject("/audit", String.class);
+        String expectedRecords = IOUtils.toString(getClass().getResourceAsStream("/expected.json"));
+
+        JSONAssert.assertEquals(auditRecords, expectedRecords, false);
     }
 
 }
