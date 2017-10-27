a5cde6b5130182f52865a34c62f7e7d67810b16b
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2017-10-27 14:41:25 +0100

    Add alerter status on or off

--- src/main/java/uk/gov/digital/ho/pttg/alert/AppropriateUsageChecker.java
+++ src/main/java/uk/gov/digital/ho/pttg/alert/AppropriateUsageChecker.java
@@ -1,5 +1,6 @@
 package uk.gov.digital.ho.pttg.alert;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.digital.ho.pttg.AuditEntryJpaRepository;
 import uk.gov.digital.ho.pttg.alert.sysdig.SuspectUsage;
@@ -14,7 +15,7 @@ public class AppropriateUsageChecker {
     private final MatchingFailureCheck matchingFailureCheck;
     private final boolean alerterStatus;
 
-    public AppropriateUsageChecker(AuditEntryJpaRepository repository, Alerter alerter, IndividualVolumeCheck individualVolumeCheck, TimeOfRequestCheck timeOfRequestCheck, MatchingFailureCheck matchingFailureCheck, boolean alerterStatus) {
+    public AppropriateUsageChecker(AuditEntryJpaRepository repository, Alerter alerter, IndividualVolumeCheck individualVolumeCheck, TimeOfRequestCheck timeOfRequestCheck, MatchingFailureCheck matchingFailureCheck, @Value("${alert.enabled}")boolean alerterStatus) {
         this.repository = repository;
         this.alerter = alerter;
         this.individualVolumeCheck = individualVolumeCheck;
