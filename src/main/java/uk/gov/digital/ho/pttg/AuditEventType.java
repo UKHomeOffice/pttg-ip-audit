d4d39b46a679a90dc412446e70aed37df5be31c7
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2017-11-01 07:58:22 +0000

    Pttg 519 (#14)
    * PTTG-519: Transplant audit creation from pttg-ip-api
    
    * PTTG-519: Introduce transactions into integration tests
    
    * PTTG-519: Diagnose integration test problem
    
    * PTTG-519: See if Spring transaction helps
    
    * PTTG-519: See if Spring transaction helps
    
    * PTTG-519: See if earlier hsqldb version works
    
    * PTTG-519: Use jersey dependency for ImmutableMap
    
    * PTTG-519: Add pagination
    
    * configure json mapping to accept localdatetime strings
    
    * PTTG-519: Address review comment
    
    * PTTG-519: Address review comment
    
    * PTTG-519: Review comment suggested introduced an interface. Did that then thought better of it.
    
    * PTTG-519: Remove seeded data from integration test.
    
    * PTTG-519: Temporarily remove the call to the service.
    
    * PTTG-519: Use SqlGroup to seed data
    
    * PTTG-519: Force a build
    
    * PTTG-519: Remove time dependency in test. Remove persisted data in integration tests.
    
    * PTTG-519: Address review comments
    
    * PTTG-519: Add alertable flag on AuditEventType enum.
    
    * PTTG-519: Execute usage checker for alertable audit data only.
    
    * PTTG-519: Add extra event type
    
    * PTTG-519: Add extra event type
    
    * PTTG-519: Add extra event type test

--- src/main/java/uk/gov/digital/ho/pttg/AuditEventType.java
+++ src/main/java/uk/gov/digital/ho/pttg/AuditEventType.java
@@ -3,6 +3,8 @@ package uk.gov.digital.ho.pttg;
 public enum AuditEventType {
 
     HMRC_INCOME_REQUEST(false),
+    HMRC_ACCESS_CODE_REQUEST(false),
+
 
     INCOME_PROVING_FINANCIAL_STATUS_REQUEST(true),
     INCOME_PROVING_FINANCIAL_STATUS_RESPONSE(true),
