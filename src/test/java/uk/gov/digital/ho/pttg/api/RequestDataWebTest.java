8173c0f272b3efac2ab170dce048aec863551bdb
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-09-17 15:31:12 +0100

    EE-7499 Register the logging interceptor or it doesn't work.  I know.

---
+++ src/test/java/uk/gov/digital/ho/pttg/api/RequestDataWebTest.java
@@ -0,0 +1,35 @@
+package uk.gov.digital.ho.pttg.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import uk.gov.digital.ho.pttg.AuditService;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = AuditResource.class, secure = false)
+public class RequestDataWebTest {
+
+    @MockBean
+    AuditService auditService;
+
+    @Autowired
+    private RequestMappingHandlerMapping handlerMapping;
+
+
+    @Test
+    public void shouldReturnHttpOkForGet() {
+        List<Object> interceptors = (List<Object>)ReflectionTestUtils.getField(handlerMapping, "interceptors");
+        assertThat(interceptors.stream().filter(i -> i.getClass().equals(RequestData.class)).count()).isEqualTo(1);
+
+    }
+
+}
