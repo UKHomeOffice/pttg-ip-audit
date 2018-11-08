9e3595559a9c61e34cdde82dc28007c6ebafaab3
Leigh Kirk <leighkirk@digital.homeoffice.gov.uk>
2018-11-08 14:10:42 +0000

    EE-10256 Log request duration on exceptions

--- src/main/java/uk/gov/digital/ho/pttg/application/ResourceExceptionHandler.java
+++ src/main/java/uk/gov/digital/ho/pttg/application/ResourceExceptionHandler.java
@@ -7,12 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import uk.gov.digital.ho.pttg.api.RequestData;
 
 import static net.logstash.logback.argument.StructuredArguments.value;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.digital.ho.pttg.api.RequestData.REQUEST_DURATION_MS;
 import static uk.gov.digital.ho.pttg.application.LogEvent.EVENT;
 import static uk.gov.digital.ho.pttg.application.LogEvent.PTTG_AUDIT_FAILURE;
 
@@ -20,15 +22,26 @@ import static uk.gov.digital.ho.pttg.application.LogEvent.PTTG_AUDIT_FAILURE;
 @Slf4j
 public class ResourceExceptionHandler {
 
+    private RequestData requestData;
+
+    public ResourceExceptionHandler(RequestData requestData) {
+        this.requestData = requestData;
+    }
+
+
     @ExceptionHandler
     public ResponseEntity handle(Exception e) {
-        log.error("Audit Exception: {}", e.getMessage(), value(EVENT, PTTG_AUDIT_FAILURE));
+        log.error("Audit Exception: {}", e.getMessage(),
+                value(EVENT, PTTG_AUDIT_FAILURE),
+                value(REQUEST_DURATION_MS, requestData.calculateRequestDuration()));
         return errorResponse(e.getMessage(), INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     public ResponseEntity handle(HttpMessageNotReadableException e) {
-        log.error("Audit Exception: {}", e.getMessage(), value(EVENT, PTTG_AUDIT_FAILURE));
+        log.error("Audit Exception: {}", e.getMessage(),
+                value(EVENT, PTTG_AUDIT_FAILURE),
+                value(REQUEST_DURATION_MS, requestData.calculateRequestDuration()));
         return errorResponse(e.getMessage(), BAD_REQUEST);
     }
 
