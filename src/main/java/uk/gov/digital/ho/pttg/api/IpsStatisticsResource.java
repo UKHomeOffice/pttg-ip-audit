8b0890341504c0cf843335b3fbf055304d79544c
Owain Roberts <owain.roberts@bjss.com>
2019-07-26 08:56:44 +0100

    EE-20105 Added logs for entry and exit to the IpsStatistics resource

--- src/main/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResource.java
+++ src/main/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResource.java
@@ -1,28 +1,41 @@
 package uk.gov.digital.ho.pttg.api;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.gov.digital.ho.pttg.IpsStatisticsService;
 
 import java.time.LocalDate;
 
+import static net.logstash.logback.argument.StructuredArguments.value;
 import static uk.gov.digital.ho.pttg.IpsStatisticsService.NO_STATISTICS;
+import static uk.gov.digital.ho.pttg.application.LogEvent.*;
 
 @AllArgsConstructor
+@Slf4j
 public class IpsStatisticsResource {
 
     private IpsStatisticsService service;
 
     public ResponseEntity<IpsStatistics> getIpsStatistics(LocalDate fromDate, LocalDate toDate) {
+        log.info("Requested IPS Statistics with fromDate={} and toDate={}", fromDate, toDate,
+                 value(EVENT, PTTG_AUDIT_GET_IPS_STATS_REQUEST_RECEIVED));
+
         IpsStatistics ipsStatistics = service.getIpsStatistics(fromDate, toDate);
         if(ipsStatistics == NO_STATISTICS) {
+            log.warn("IPS Statistics not found", value(EVENT, PTTG_AUDIT_GET_IPS_STATS_NOT_FOUND));
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
+        log.info("Returned IPS Statistics", value(EVENT, PTTG_AUDIT_GET_IPS_STATS_RESPONSE_SUCCESS));
         return new ResponseEntity<>(ipsStatistics, HttpStatus.OK);
     }
 
     public void storeIpsStatistics(IpsStatistics ipsStatistics) {
+        log.info("Request to store IPS Statistics for fromDate={} and toDate={}", ipsStatistics.fromDate(), ipsStatistics.toDate(),
+                 value(EVENT, PTTG_AUDIT_STORE_IPS_STATS_REQUEST_RECEIVED));
         service.storeIpsStatistics(ipsStatistics);
+        log.info("Successfully stored IPS Statistics", value(EVENT, PTTG_AUDIT_STORE_IPS_STATS_RESPONSE_SUCCESS));
     }
 }
--- src/test/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResourceTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResourceTest.java
@@ -1,22 +1,33 @@
 package uk.gov.digital.ho.pttg.api;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
+import net.logstash.logback.marker.ObjectAppendingMarker;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.gov.digital.ho.pttg.IpsStatisticsService;
+import uk.gov.digital.ho.pttg.application.LogEvent;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.atLeastOnce;
 import static uk.gov.digital.ho.pttg.IpsStatisticsService.NO_STATISTICS;
+import static uk.gov.digital.ho.pttg.application.LogEvent.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IpsStatisticsResourceTest {
@@ -25,6 +36,10 @@ public class IpsStatisticsResourceTest {
 
     @Mock
     private IpsStatisticsService mockStatisticsService;
+    @Mock
+    private Appender<ILoggingEvent> mockAppender;
+
+    private ArgumentCaptor<LoggingEvent> logCaptor;
 
     private IpsStatisticsResource resource;
     private static final int ANY_INT = 5;
@@ -32,6 +47,11 @@ public class IpsStatisticsResourceTest {
     @Before
     public void setUp() {
         resource = new IpsStatisticsResource(mockStatisticsService);
+
+        Logger logger = (Logger) LoggerFactory.getLogger(IpsStatisticsResource.class);
+        logger.addAppender(mockAppender);
+
+        logCaptor = ArgumentCaptor.forClass(LoggingEvent.class);
     }
 
     @Test
@@ -74,17 +94,42 @@ public class IpsStatisticsResourceTest {
 
     @Test
     public void getIpsStatistics_givenDates_logEntry() {
-        fail("TODO OJR EE-20105 2019-07-25");
+        LocalDate someFromDate = LocalDate.parse("2019-01-01");
+        LocalDate someToDate = LocalDate.parse("2019-01-31");
+        resource.getIpsStatistics(someFromDate, someToDate);
+
+        then(mockAppender).should(atLeastOnce()).doAppend(logCaptor.capture());
+
+        LoggingEvent loggingEvent = getLoggingEvent(PTTG_AUDIT_GET_IPS_STATS_REQUEST_RECEIVED);
+        assertThat(loggingEvent.getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvent.getFormattedMessage()).contains("Request", "IPS Statistics", someFromDate.toString(), someToDate.toString());
     }
 
     @Test
     public void getIpsStatistics_statsFound_logReturn() {
-        fail("TODO OJR EE-20105 2019-07-25");
+        IpsStatistics someStatistics = new IpsStatistics(ANY_DATE, ANY_DATE, ANY_INT, ANY_INT, ANY_INT, ANY_INT);
+        given(mockStatisticsService.getIpsStatistics(any(), any())).willReturn(someStatistics);
+
+        resource.getIpsStatistics(ANY_DATE, ANY_DATE);
+
+        then(mockAppender).should(atLeastOnce()).doAppend(logCaptor.capture());
+
+        LoggingEvent loggingEvent = getLoggingEvent(PTTG_AUDIT_GET_IPS_STATS_RESPONSE_SUCCESS);
+        assertThat(loggingEvent.getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvent.getFormattedMessage()).contains("Returned", "IPS Statistics");
     }
 
     @Test
     public void getStatistics_noStatsFound_logReturn() {
-        fail("TODO OJR EE-20105 2019-07-25");
+        given(mockStatisticsService.getIpsStatistics(any(), any())).willReturn(NO_STATISTICS);
+
+        resource.getIpsStatistics(ANY_DATE, ANY_DATE);
+
+        then(mockAppender).should(atLeastOnce()).doAppend(logCaptor.capture());
+
+        LoggingEvent loggingEvent = getLoggingEvent(PTTG_AUDIT_GET_IPS_STATS_NOT_FOUND);
+        assertThat(loggingEvent.getLevel()).isEqualTo(Level.WARN);
+        assertThat(loggingEvent.getFormattedMessage()).contains("IPS Statistics", "not found");
     }
 
     @Test
@@ -98,11 +143,37 @@ public class IpsStatisticsResourceTest {
 
     @Test
     public void storeIpsStatistics_givenStats_logEntry() {
-        fail("TODO OJR EE-20105 2019-07-25");
+        LocalDate someFromDate = LocalDate.parse("2019-04-01");
+        LocalDate someToDate = LocalDate.parse("2019-04-30");
+        IpsStatistics someStatistics = new IpsStatistics(someFromDate, someToDate, ANY_INT, ANY_INT, ANY_INT, ANY_INT);
+
+        resource.storeIpsStatistics(someStatistics);
+
+        then(mockAppender).should(atLeastOnce()).doAppend(logCaptor.capture());
+
+        LoggingEvent loggingEvent = getLoggingEvent(PTTG_AUDIT_STORE_IPS_STATS_REQUEST_RECEIVED);
+        assertThat(loggingEvent.getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvent.getFormattedMessage()).contains("Request", "store", "IPS Statistics", someToDate.toString(), someFromDate.toString());
     }
 
     @Test
     public void storeIpsStatistics_anyStats_logReturn() {
-        fail("TODO OJR EE-20105 2019-07-25");
+        IpsStatistics anyStatistics = new IpsStatistics(ANY_DATE, ANY_DATE, ANY_INT, ANY_INT, ANY_INT, ANY_INT);
+
+        resource.storeIpsStatistics(anyStatistics);
+
+        then(mockAppender).should(atLeastOnce()).doAppend(logCaptor.capture());
+
+        LoggingEvent loggingEvent = getLoggingEvent(PTTG_AUDIT_STORE_IPS_STATS_RESPONSE_SUCCESS);
+        assertThat(loggingEvent.getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvent.getFormattedMessage()).contains("Success", "store", "IPS Statistics");
+    }
+
+    public LoggingEvent getLoggingEvent(LogEvent logEvent) {
+        ObjectAppendingMarker marker = new ObjectAppendingMarker("event_id", logEvent);
+        return logCaptor.getAllValues().stream()
+                        .filter(log -> ArrayUtils.contains(log.getArgumentArray(), marker))
+                        .findFirst()
+                        .orElseThrow(AssertionError::new);
     }
 }
