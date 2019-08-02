e9a23e14147bc94627a4e7cfc58b211fa3ebe26e
Owain Roberts <owain.roberts@bjss.com>
2019-08-02 16:19:07 +0100

    EE-21310 Added @DirtiesContext annotation to IpsStatisticsResourceIntTest to try and stop inermittent test failure

--- src/test/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResourceIntTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResourceIntTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
@@ -15,6 +16,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@DirtiesContext
 public class IpsStatisticsResourceIntTest {
 
     @Autowired
