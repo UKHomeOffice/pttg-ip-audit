f77a8e5d4d631b3e329046ea39a12284e56d6601
Owain Roberts <owain.roberts@bjss.com>
2019-08-05 09:18:10 +0100

    EE-21310 Removing unused import

--- src/test/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResourceIntTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/api/IpsStatisticsResourceIntTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.digital.ho.pttg.AuditEntryJpaRepository;
 
