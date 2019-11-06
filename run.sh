61baf4129b5a3e8098bdfe1c86080b8bb25dfa9d
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-11-06 11:58:48 +0000

    EE-28262 Create empty truststore to see if RDS works without it

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 NAME=${NAME:-pttg-ip-audit}
 
+mv /app/truststore.jks /tmp
+keytool -genkey -noprompt -alias ks1 -dname "CN=testCN, OU=testOU, O=testO, L=testL, S=testS, C=testC" -keystore /app/truststore.jks -storepass storepass -keypass keypass
+
 JAR=$(find . -name ${NAME}*.jar|head -1)
 java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
 
