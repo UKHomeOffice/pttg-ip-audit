df3972d785cfa2ffb63007a18c80b269539317b4
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-10-15 14:50:08 +0100

    EE-34558: Add new event type for dwp-service

--- src/test/java/uk/gov/digital/ho/pttg/AuditEventTypeTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/AuditEventTypeTest.java
@@ -10,29 +10,37 @@ public class AuditEventTypeTest {
 
     @Test
     public void shouldBeKnowItems() {
-        assertThat(AuditEventType.values().length).isEqualTo(15);
+        assertThat(AuditEventType.values().length).isEqualTo(16);
     }
 
     @Test
     public void shouldBeAlertable() {
         assertThat(INCOME_PROVING_FINANCIAL_STATUS_REQUEST.isAlertable()).isTrue();
         assertThat(INCOME_PROVING_FINANCIAL_STATUS_RESPONSE.isAlertable()).isTrue();
+
         assertThat(INCOME_PROVING_INCOME_CHECK_REQUEST.isAlertable()).isTrue();
         assertThat(INCOME_PROVING_INCOME_CHECK_RESPONSE.isAlertable()).isTrue();
     }
 
     @Test
     public void shouldNotBeAlertable() {
+
         assertThat(HMRC_INCOME_REQUEST.isAlertable()).isFalse();
         assertThat(HMRC_ACCESS_CODE_REQUEST.isAlertable()).isFalse();
+
         assertThat(DWP_BENEFIT_REQUEST.isAlertable()).isFalse();
+
         assertThat(DWP_NINO_VALIDATE_REQUEST.isAlertable()).isFalse();
         assertThat(DWP_NINO_TRACE_ALLOCATE_REQUEST.isAlertable()).isFalse();
+        assertThat(DWP_NINO_TRACE_ALLOCATE_DATA_MANIPULATION.isAlertable()).isFalse();
+
         assertThat(RESIDENCE_PROVING_INCOME_REQUEST.isAlertable()).isFalse();
         assertThat(RESIDENCE_PROVING_INCOME_RESPONSE.isAlertable()).isFalse();
         assertThat(RESIDENCE_PROVING_BENEFITS_REQUEST.isAlertable()).isFalse();
         assertThat(RESIDENCE_PROVING_BENEFITS_RESPONSE.isAlertable()).isFalse();
+
         assertThat(ARCHIVED_RESULTS.isAlertable()).isFalse();
+
         assertThat(IPS_STATISTICS.isAlertable()).isFalse();
     }
 }
